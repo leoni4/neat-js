766053f55a98bc6d34e745cdd7b9065984e3edf1
leonid.lilo <leonid.lilo@gmail.com>
2022-05-26 23:57:55 +0300

    neat added logic

--- src/neat/neat.ts
+++ src/neat/neat.ts
@@ -1,5 +1,30 @@
+import { ConnectionGene } from '../genome';
+import { RandomHashSet } from '../dataStructures/randomHashSet';
+
 export class Neat {
     static get MAX_NODES(): number {
         return Math.pow(2, 20);
+    }
+
+    #inputNodes: number;
+    #outputNodes: number;
+    #clients: number;
+
+    #allConnections: Map<ConnectionGene, ConnectionGene> = new Map<ConnectionGene, ConnectionGene>();
+    #allNodes: RandomHashSet = new RandomHashSet();
+
+    constructor(inputNodes: number, outputNodes: number, clients: number) {
+        this.#inputNodes = inputNodes;
+        this.#outputNodes = outputNodes;
+        this.#clients = clients;
+    }
+
+    reset(inputNodes: number, outputNodes: number, clients: number) {
+        this.#inputNodes = inputNodes;
+        this.#outputNodes = outputNodes;
+        this.#clients = clients;
+
+        this.#allConnections.clear();
+        this.#allNodes.clear();
     }
 }
