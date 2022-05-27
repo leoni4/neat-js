b9b80255c5dbca80456ada30dcd0d3a1389a53cd
leonid.lilo <leonid.lilo@gmail.com>
2022-05-27 12:56:48 +0300

    webpack config added

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-undef,@typescript-eslint/no-var-requires */
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+
+    devtool: 'source-map',
+
+    entry: './src/index.ts',
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+    },
+
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+
+    watch: true,
+};
