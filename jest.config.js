9fd733af22bfc9c2637a0853d6beb0728bb6b012
leonid.lilo <leonid.lilo@12go.asia>
2026-01-12 20:13:01 +0300

    chore: deleted jest tests

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-// eslint-disable-next-line no-undef
-export default {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    collectCoverage: true,
-    collectCoverageFrom: ['<rootDir>/src/**/*'],
-};
--- tests/sanity.test.ts
+++
@@ -1,5 +0,0 @@
-describe('sanity test', () => {
-    it('setup correctly', () => {
-        expect(true).toBe(true);
-    });
-});
